26d39bc054520771df0f090580ec1e6f516f3646
Aidan Nachi <aidannachi@gmail.com>
2024-05-09 11:07:17 -0500

    Added a second deck of cards, and a funciton that simulates a game of Texas Hold'em and returns the winning hand so we can later visualize this data.

--- hold_em.py
+++ hold_em.py
@@ -21,10 +21,11 @@ class hold_em():
     def __init__(self):
         """ Initialize a game of Texas Hold'em. """
 
-        # Create a deck of playing cards. 
+        # Create two alternating decks of playing cards. 
         # Deck key
         # 11 = Jack, 12 = Queen, 13 = King, 14 = Ace
         self.deck = list(itertools.product(range(2, 15), ['Spade', 'Heart', 'Diamond', 'Club']))
+        self.second_deck = list(itertools.product(range(2, 15), ['Spade', 'Heart', 'Diamond', 'Club']))
 
     
     def holdem_simulation(self, players_hand, num_other_players, num_of_folding_players=0):
@@ -84,6 +85,50 @@ class hold_em():
         # Find the winning hand.
         win_finder = Win_finder()
         return win_finder.game_result(players_hand, other_players_hands, board)
+    
+
+    def holdem_simulation_winning_hand(self, num_of_players):
+        """ Simulate a game of Texas Holdem and get the winning hand. """
+
+        # Make a copy and shuffle the deck to play.
+        second_playing_deck = self.second_deck.copy()
+        np.random.shuffle(second_playing_deck)
+
+        # Create players to and deal them hole cards so we can find the winner.
+        players_hands = []
+        for i in range(num_of_players):
+            players_hands.append([second_playing_deck.pop(0), second_playing_deck.pop(0)])
+
+
+        # Simulate the flop, turn, and river to get the board.
+        # ----------------------------------------------------
+        # Take 4 cards from the top of the deck, burn one and flip three.
+        flop = second_playing_deck[0:4]
+        del second_playing_deck[0:4]
+        del flop[0]
+        
+        # Take two cards from the top of the deck, burn and turn the
+        # turn card (fourth community card).
+        turn = second_playing_deck[0:2]
+        del second_playing_deck[0:2]
+        del turn[0]
+
+        # Take two cards from the top of the deck, then burn one and turn
+        # the river card (last community card).
+        river = second_playing_deck[0:2]
+        del second_playing_deck[0:2]
+        del river[0]
+
+        # The sum of cards from the flop, turn, and river make up the board.
+        board = flop + turn + river
+
+
+        # Return the winning hand of the game simulation.
+        win_finder = Win_finder()
+        return win_finder.winning_result()
+
+
+
 
         
 
